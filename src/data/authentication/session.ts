d356e0feb090d8891724f1588892dfe229f20896
wmatos <willianssantos227@gmail.com>
2025-08-26 14:37:18 -0300

    feat: add  component button login, google session

--- src/data/authentication/session.ts
+++ src/data/authentication/session.ts
@@ -3,10 +3,18 @@ import "server-only";
 import { headers } from "next/headers";
 
 import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
 
 export const getSessionUser = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   return session;
+};
+
+export const handleSignInWithGoogle = async () => {
+  const google = await authClient.signIn.social({
+    provider: "google",
+  });
+  return google;
 };
