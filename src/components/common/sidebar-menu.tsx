3d80c2321c731703e2ed9006645a82b16b54dde1
wmatos <willianssantos227@gmail.com>
2025-08-28 09:30:27 -0300

    feat: Integrated calls to sidebar-menu component categories.

--- src/components/common/sidebar-menu.tsx
+++ src/components/common/sidebar-menu.tsx
@@ -1,18 +1,24 @@
-import {
-  HomeIcon,
-  LogOutIcon,
-  PackageIcon,
-  ShoppingBagIcon,
-} from "lucide-react";
+"use client";
+
+import { HomeIcon, LogOutIcon, PackageIcon } from "lucide-react";
 import Link from "next/link";
 
 import { authClient } from "@/lib/auth-client";
 
 export function SidebarMenu() {
-    const { data: session } = authClient.useSession();
+  const { data: session } = authClient.useSession();
+
+  const categories = [
+    { name: "Camisetas", slug: "camisetas" },
+    { name: "Bermuda & Shorts", slug: "bermuda-shorts" },
+    { name: "Calças", slug: "calcas" },
+    { name: "Jaquetas & Moletons", slug: "jaquetas-moletons" },
+    { name: "Tênis", slug: "tenis" },
+    { name: "Acessórios", slug: "acessorios" },
+  ];
 
   return (
-    <div className="flex flex-col space-y-4 text-sm font-medium">
+    <div className="mt-5 flex flex-col space-y-4 text-sm font-medium">
       {/* Top Items */}
       <Link href="/" className="flex items-center gap-2 hover:text-black">
         <HomeIcon className="h-4 w-4" />
@@ -27,47 +33,34 @@ export function SidebarMenu() {
         Meus Pedidos
       </Link>
 
-      <Link href="/cart" className="flex items-center gap-2 hover:text-black">
-        <ShoppingBagIcon className="h-4 w-4" />
-        Sacola
-      </Link>
-
       <hr className="my-4" />
 
-      {/* Categorias */}
-
-      <Link href="category/camisetas" className="hover:text-black">
-        Camisetas
-      </Link>
-      <Link href="/bermudas" className="hover:text-black">
-        Bermuda & Shorts
-      </Link>
-      <Link href="/calcas" className="hover:text-black">
-        Calças
-      </Link>
-      <Link href="/jaquetas" className="hover:text-black">
-        Jaquetas & Moletons
-      </Link>
-      <Link href="/tenis" className="hover:text-black">
-        Tênis
-      </Link>
-      <Link href="/acessorios" className="hover:text-black">
-        Acessórios
-      </Link>
+      {categories.map((category) => {
+        return (
+          <Link
+            key={category.slug}
+            href={`/category/${category.slug}`}
+            className={"hover:text-primary font-semibold text-black"}
+          >
+            {category.name}
+          </Link>
+        );
+      })}
 
       <hr className="my-4" />
 
       {/* Logout */}
       {session?.user && (
         <button
-        className="flex items-center gap-2 text-gray-500 hover:text-red-600"
-        onClick={() => authClient.signOut()}
-      >
-        <LogOutIcon />
-        Sair da conta
-      </button>
+          className="flex items-center gap-2 text-gray-500 hover:text-red-600"
+          onClick={() => authClient.signOut()}
+        >
+          <LogOutIcon />
+          Sair da conta
+        </button>
       )}
     </div>
   );
 }
+
 export default SidebarMenu;
