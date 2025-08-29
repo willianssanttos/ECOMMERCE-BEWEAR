2715c4fd51285fa4a6fc91c7da0e7007ad95a709
wmatos <willianssantos227@gmail.com>
2025-08-29 12:52:50 -0300

    refactor: Performed sidebar-menu refactoring, separating logout into a separate component

---
+++ src/components/common/sign-out.tsx
@@ -0,0 +1,25 @@
+import { LogOutIcon } from "lucide-react";
+
+import { authClient } from "@/lib/auth-client";
+
+const SignOut = () => {
+  const { data: session } = authClient.useSession();
+  return (
+    <div className="mt-12">
+      <div className="flex flex-col space-y-4 text-sm font-medium">
+        {/* Logout */}
+        {session?.user && (
+          <button
+            className="mt-15 flex items-center gap-2 text-gray-500 hover:text-red-600"
+            onClick={() => authClient.signOut()}
+          >
+            <LogOutIcon />
+            Sair da conta
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SignOut;
